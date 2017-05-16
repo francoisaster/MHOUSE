6214ec974f563ad0fba07f77b7e49f0469231a6d
martin allainguillaume <martin allainguillaume>
2017-05-16 19:53:47 +0200

    connexionbis retouche

--- Autres/connexionbis.php
+++ Autres/connexionbis.php
@@ -1,5 +1,13 @@
 <?php
+ 
 
+$bdd= new PDO("mysql:host=localhost;dbname=m'house",'root','root');
+/*$rech=$bdd->query("SELECT * FROM utilisateur");
+var_dump($rech->fetchAll());*/
+
+
+if(isset($_POST['submit']));{
+   
     $email=htmlentities(trim($_POST['mail']));
     $mdp=sha1($_POST['mdp']);
     $mdp2=sha1($_POST['mdp2']);
@@ -15,47 +23,48 @@
 
     
 
-    if(!empty($_POST['mail']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'] ))
-    {   
-                $reponse = $bdd->query('SELECT pseudo,email, mdp FROM utilisateur WHERE pseudo="'.$pseudo.'"');
+    if(!empty($_POST['mail']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'] ))    
+    {
+       
+         if($mdp== $mdp2)
+            {
+                /*pseudo="'.$pseudo.'" mdp="'.$mdp.'"*/
+                $reponse = $bdd->query('SELECT pseudo,email, mdp FROM utilisateur WHERE email="'.$email.'" '); // il reste un pb avec le WHERE ou j'arrive pas a faire plusieurs requetes simultaness, le AND ne marche pas !
                 if($reponse->rowcount()==0){
                 // l'utilisateur n'est pas dans la base de donnée
                     $insertmbr= $bdd->prepare("INSERT INTO utilisateur(compte,pseudo,email,mdp,sexe,nom,prenom,age,adresse) VALUES(?,?,?,?,?,?,?,?,?)");
                     $insertmbr->execute(array($compte,$pseudo,$email,$mdp,$civilite,$nom,$prenom,$age,$adresse));
                     $erreur= "Votre compte a bien été crée"; 
                 }
-                //a enlever et mettre en page connexion
-                else{
-                // utilisateur trouvé dans la base de données
-                        $ligne = $reponse->fetch();
-                        if($mdp!=$ligne['mdp']){
-                        // Le mot de passe entré ne correspond pas à celui stocké dans la base de données
-                                    $erreur = "Mot de passe incorrect";
-                                    include("Vue/app_accueil.php");
-                        } 
-                         else{ 
-                        // mot de passe correct, on affiche la page d'accueil
-                                    $_SESSION["userID"] = $ligne['id'];
-                                    include("Vue/page_accueil.php");
-                            }
-                    }      
+                else
+                {
+                // compte deja existant
+                    $erreur="Veuillez saisir de nouveaux identifiants";
 
-
-                }           
+                        
+                }     
                 
-}
+            }
+            else
+            {
+            // Cas ou les mots de passes doivent etre identiques
+
+                $erreur=" Vos mots de passes doivent être identiques";
+            }
+    
+    }
     
     else
     {
         $erreur="Tout les champs doivent être saisis";
-        include("Vue/page_accueil.php");
 
     }
 
+}
  if(isset($erreur))
     {
-        echo '<font color="red">' .$erreur. "</font>";
-        include("Vue/page_accueil.php");
+        echo '<font color="red">' .$erreur. '</font>';
+        include("../Vue/page_accueil.php");
     }
    
 
