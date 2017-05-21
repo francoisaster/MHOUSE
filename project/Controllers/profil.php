7923f980160413c735091e27401640c804c260de
Aster <francois.le.pape@gmail.com>
2017-05-22 00:21:26 +0200

    modif profil

--- project/Controllers/profil.php
+++ project/Controllers/profil.php
@@ -7,6 +7,23 @@
  */
 
 require '../Models/profil.php';
-require '../Models/connexion.php'; //pour avoir getUser
+
+/*
 updateProfil();
+*/
+upContact();
+
+
+$requser = getUserSelected(); // Le reqUser contient requser2 qui a la querry bdd, récupère toutes les données de l'utilisateur connecté
+$donnees = $requser->fetch();
+
+    $_SESSION['pseudo'] = $donnees['pseudo'];
+    $_SESSION['nom'] = $donnees['nom'];
+    $_SESSION['prenom'] = $donnees['prenom'];
+    $_SESSION['adresse'] = $donnees['adresse'];
+    $_SESSION['email'] = $donnees['email'];
+    $_SESSION['sexe'] = $donnees['sexe'];
+    $_SESSION['pass'] = $donnees['pass'];
+    $_SESSION['date_naissance'] = $donnees['date_naissance'];
 header('Location:../public/index.php?p=profil');
+
--- project/Models/profil.php
+++ project/Models/profil.php
@@ -6,10 +6,9 @@
  * Time: 15:16
  */
 
-
 require'../Models/connexionBdd.php';
-function afficheProfil(){
 
+function afficheProfil(){
     $bdd=connexionBdd();
     $req = $bdd->prepare('SELECT pseudo, prenom, nom, adresse, sexe, date_naissance, email FROM utilisateur
 WHERE id_utilisateur= ? ');
@@ -24,14 +23,16 @@ WHERE id_utilisateur= ? ');
     $req->closeCursor();
 }
 
-
-
+/*
 function updateProfil()
 {
-
     $bdd=connexionBdd();
-    $req = $bdd->prepare('UPDATE utilisateur SET pseudo = :nv_pseudo, prenom=:nv_prenom, nom=:nv_nom, adresse=:nv_adresse, sexe=:nv_sexe, date_naissance=:date_naissance, email=:nv_email WHERE id_utilisateur=:id_utilisateur');
-    $req = execute(array(
+    $requpdate = $bdd->prepare('
+UPDATE utilisateur
+SET pseudo = :nv_pseudo, prenom = :nv_prenom, nom = :nv_nom, adresse = :nv_adresse, sexe = :nv_sexe,
+date_naissance = :date_naissance, email = :nv_email, numero_tel = :numero_tel
+WHERE id_utilisateur = :id_utilisateur ');
+    $requpdate->execute(array(
         'nv_pseudo' => $_POST['pseudo'],
         'nv_prenom' => $_POST['prenom'],
         'nv_nom' => $_POST['nom'],
@@ -39,5 +40,28 @@ function updateProfil()
         'nv_sexe' => $_POST['sexe'],
         'date_naissance' => $_POST['date_naissance'],
         'nv_email' => $_POST['email'],
+        'numero_tel' => $_POST['numero_tel'],
+        'id_utilisateur' => $_SESSION['id_utilisateur'],
+        ));
+}*/
+
+function upContact(){
+    $bdd=connexionBdd();
+    $req = $bdd->prepare('UPDATE utilisateur SET adresse = :nv_adresse, email = :nv_mail, numero_tel = :nv_tel WHERE id_utilisateur = :id_utilisateur');
+    $req->execute(array(
+        'nv_adresse' => $_POST['adresse'],
+        'nv_mail' => $_POST['email'],
+        'nv_tel' => $_POST['numero_tel'],
+        'id_utilisateur' => $_SESSION['id_utilisateur'],
     ));
+
+}
+
+function getUserSelected(){
+    $bdd=connexionBdd();
+    $requser2 = $bdd->prepare('SELECT * FROM utilisateur WHERE id_utilisateur= :id_utilisateur');
+    $requser2 -> execute(array(
+    'id_utilisateur' => $_SESSION['id_utilisateur'],
+        ));
+    return $requser2;
 }
