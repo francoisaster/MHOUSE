2ea54dc19cad2e029d890edef7a5e4fe4a013536
Aster <francois.le.pape@gmail.com>
2017-06-01 09:57:18 +0200

    test

--- project/Views/pieces.php
+++ project/Views/pieces.php
@@ -45,7 +45,6 @@
     </fieldset>
 </form>
 
-<<<<<<< HEAD
 <form action="../Controllers/afficheCapteur.php" method="post" class="block">
     <fieldset>
         <legend>Voir les capteurs par piece</legend>
@@ -65,8 +64,6 @@
     </fieldset>
 </form>
 
-=======
->>>>>>> 65a1e0c181f608b825640348f681455e7b7d09d8
 <?php // A METTRE DANS CONTROLLERS
 if(isset($_SESSION['id_piece'])){
 require'../Models/afficheCapteur.php';
--- project/Views/templates/menu.php
+++
@@ -1,8 +0,0 @@
-<ul class="nav navbar-nav">
-    <li><a href="../public/index.php?p=connexion">Connexion</a></li>
-    <li><a href="../public/index.php?p=inscription">Inscription</a></li>
-    <li><a href="../public/index.php?p=pieces">Pièces</a></li>
-    <li><a href="../public/index.php?p=profil">Profil</a></li>
-    <li><a href="../public/index.php?p=contact">Contact</a></li>
-    <li><a href="../public/index.php?p=leave">Deconnexion</a></li>
-</ul>
