0db4745933a135c5eefbccc2ce235edde43518ad
Aster <francois.le.pape@gmail.com>
2017-06-26 22:46:52 +0200

    commit

--- project/Views/home.php
+++ project/Views/home.php
@@ -3,38 +3,6 @@ require '../Models/test2.php'; //necessaire pour avoir la fonction affichePieces
 
 require '../Models/connexionBdd2.php';
 $bdd = connexionBdd2();
-/*try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=mhouse_bdd;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
-*/
-?>
-<?php
-/*
-$temperature = 'temperature';
-$_SESSION['id_utilisateur'] = 34;
-$req = $bdd -> prepare('SELECT nom_capteur FROM capteurs WHERE id_utilisateur = ? AND type_capteur = ?ORDER BY id_capteur');
-$req->execute(array($_SESSION['id_utilisateur'],$temperature));
-*/
-/*
-$requser = $bdd->prepare("
-SELECT valeurs_capteur.date_mesure, valeurs_capteur.valeur, capteurs.id_capteur, capteurs.nom_capteur
-FROM valeurs_capteur
-INNER JOIN capteurs
-ON valeurs_capteur.id_capteur = capteurs.id_capteur
-WHERE capteurs.id_utilisateur = ?
-ORDER BY valeurs_capteur.date_mesure
-");
-$requser->execute(array($_SESSION['id_utilisateur'],));
-*/
-/*
-if(isset($_SESSION['capteur_stats'])){
-    $capteur_choisi2 = $_SESSION['capteur_stats'];
-}*/
 
 if(isset($_POST['capteur_choisi'])) {
     $capteur_choisi = $_POST['capteur_choisi'];
@@ -50,50 +18,19 @@ if(isset($_POST['capteur_choisi'])) {
     $result->execute(array($idCapteur));
 
 
-
 $reponses = $bdd->query('SELECT AVG(valeur) AS valeur FROM valeurs_capteur');
 while ($donnees42 = $reponses->fetch())
 {
     $moyenne = $donnees42['valeur'];
 }
 $reponses->closeCursor();
-
-
 }
-/*
-if(isset($_POST['capteur_choisi'])) {
-    $capteur_choisi = $_POST['capteur_choisi'];
 
-
-    $test = $bdd->prepare('SELECT id_capteur FROM capteurs WHERE nom_capteur = ? AND id_utilisateur= ?'); // rajout d'heure
-    $test->execute(array($capteur_choisi, $_SESSION['id_utilisateur']));
-    while ($donneeTest = $test->fetch()) {
-        $idCapteur = htmlspecialchars($donneeTest['id_capteur']);
-    }
-
-    $result = $bdd->prepare('SELECT * FROM valeurs_capteur WHERE id_capteur= ?'); // rajout d'heure
-    $result->execute(array($idCapteur));
-}*/
-?>
-
-<?php
 $reponseDerniere = $bdd->query('SELECT valeur FROM valeurs_capteur ORDER BY date_mesure DESC');
 $donnees102 = $reponseDerniere->fetch();
 $luminositéRecue = $donnees102['valeur'];
 $reponseDerniere->closeCursor();
 ?>
-
-<!--
-<div class="block" id="demo" onclick="myFunction()">
-<p  > Il n'y a aucunes notifications</p>
-</div>
-
-<script>
-    function myFunction() {
-        document.getElementById("demo").style.display='none';
-    }
-</script>
--->
 
 <!--
  ICI Les notifications
